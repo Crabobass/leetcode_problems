c5867cfdae4cb29af444ae040b3e7ff65d6d4a99
Кузнецов Алексей Сергеевич <crabobass@gmail.com>
2023-10-24 08:56:03 +0300

    [solved] 1266. Minimum Time Visiting All Points
    https://leetcode.com/problems/minimum-time-visiting-all-points/description/

---
+++ easy/1266. Minimum Time Visiting All Points.php
@@ -0,0 +1,39 @@
+<?php
+
+class Solution
+{
+    /**
+     * @param Integer[][] $points
+     * @return Integer
+     */
+    public function minTimeToVisitAllPoints(array $points): int
+    {
+        $res = 0;
+        foreach ($points as $i => $point) {
+            if ($points[$i-1] === null)
+                continue;
+            $res += max(abs($points[$i-1][0] - $point[0]), abs($points[$i-1][1] - $point[1]));
+        }
+        return $res;
+    }
+}
+
+function test()
+{
+    $cases = [
+        ['input' => [[1, 1], [3, 4], [-1, 0]], 'output' => 7],
+        ['input' => [[3, 2], [-2, 2]], 'output' => 5],
+    ];
+
+    foreach ($cases as $case) {
+        $start  = microtime(true);
+        $output = (new Solution)->minTimeToVisitAllPoints($case['input']);
+        $result = $output === $case['output'];
+        $time   = round((microtime(true) - $start) * 100000, 3);
+        echo $result ? "TRUE" : "FALSE";
+        echo "[exp: {$case['output']}; output: $output] ";
+        echo ' ' . $time . "\r\n";
+    }
+}
+
+test();
